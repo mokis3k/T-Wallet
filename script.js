fcede4fc296bc5615044ededfe3b3acbbcf6c387
Roman Tafii <roman.tafii@gmail.com>
2024-03-02 16:31:55 +0100

    updating balance wrapper

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 import { getCurrencyRate } from "./js/cuurencyRate.js";
 
+const dateContainer = document.querySelector("#dateContainer");
+
+const renderCurrentDate = (DOMelem) => {
+  let currentDate = new Date();
+  const date = currentDate.getDate();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+  DOMelem.innerHTML = `${date < 10 ? `0${date}` : date}.${
+    month + 1 < 10 ? `0${month + 1}` : month + 1
+  }.${year}`;
+};
+
 getCurrencyRate();
+renderCurrentDate(dateContainer);
